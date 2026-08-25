a79dec3428cd9c12946078f0a4c676151a4eb312
jayson <jayson@saavylab.com>
2026-08-24 21:51:06 -0600

    Track rolling NixOS inputs

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Production NixOS configuration for the desktop";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
